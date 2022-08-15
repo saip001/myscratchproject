fff6962ac13dac3c1c3e40acc8efd2930f5927cd
saip001 <sair48387@gmail.com>
2022-08-16 01:00:11 +0530

    added webpack to dev

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(__dirname + '/public'));
 app.set('port', process.env.PORT || 3000);
 
 const server = app.listen(app.get('port'), function() {
