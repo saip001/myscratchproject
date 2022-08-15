e7e7d0879238fcb00c8da62df6fa1a4e193a0b6f
saip001 <sair48387@gmail.com>
2022-08-16 00:43:35 +0530

    added webpack

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+const path = require('path');
 const express = require('express');
-
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
 app.set('port', process.env.PORT || 3000);
-
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../scratch-starter-project/public/index.html'));
+});
 const server = app.listen(app.get('port'), function() {
   console.log('listening on port ', server.address().port);
 });
